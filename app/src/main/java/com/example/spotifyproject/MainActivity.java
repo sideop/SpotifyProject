e6134ecb47e44fc214c46a8ba233a00091797be6
sideop <s84267018201ciersh>
2019-12-05 13:21:50 -0600

    Initial commit

---
+++ app/src/main/java/com/example/spotifyproject/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.spotifyproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button login = findViewById(R.id.loginButton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent start = new Intent(getApplicationContext(), ScrollPage.class);
+                startActivity(start);
+            }
+        });
+    }
+}
